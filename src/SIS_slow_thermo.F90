cac8228d629b21bfd2b438f9a03d5bbf497000ef
Will Gregory <William.gregory@outlook.com>
2025-05-09 10:40:25 -0400

    add MLinput diag
    SW and TS network input diagnostics

--- src/SIS_slow_thermo.F90
+++ src/SIS_slow_thermo.F90
@@ -203,6 +203,8 @@ subroutine post_flux_diagnostics(IST, FIA, IOF, CS, G, US, IG, Idt_slow, ML) !WG
           ML%SW_filtered(i,j) = ML%SW_filtered(i,j) + (net_sw(i,j)*nsteps_i)
           ML%TS_filtered(i,j) = ML%TS_filtered(i,j) + (FIA%Tskin_avg(i,j)*nsteps_i)
        enddo; enddo
+       if (ML%id_swnet>0) call post_SIS_data(ML%id_swnet, ML%SW_filtered, ML%diag)
+       if (ML%id_tsnet>0) call post_SIS_data(ML%id_tsnet, ML%TS_filtered, ML%diag)
     endif
     if (FIA%id_albedo>0) then
       do j=jsc,jec ; do i=isc,iec
